0b84003863edd798870689b862977c9349393c5b
Minhajul43 <minhajul255685islam@gmail.com>
2026-01-31 21:57:33 +0600

    call function

---
+++ call-fn.js
@@ -0,0 +1,18 @@
+// declare the function
+
+function study() {
+  console.log('Set your mind for study');
+  console.log('Then take the book and set the table');
+  console.log('Now set a plan and start studying');
+}
+
+// call function!
+// study();
+
+function sleep() {
+  console.log('Finished your necessary work!');
+  console.log('Goo to bathroom and fresh yourself!');
+  console.log('silent your phone and ready to sleep.');
+}
+// call function!
+sleep();
